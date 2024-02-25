28374fe48bdaa8a28b0062a5d5ae0be230916f10
Gilberto 2 <gilberto.gonzalez04@utrgv.edu>
2024-02-25 16:38:13 -0600

    Ai image generation commented out

---
+++ Assets/imageGeneration.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//import libraries
+using OpenAI_API;
+using OpenAI_API.Images;
+using OpenAI_API.Models;
+using System.Threading.Tasks;
+using System;
+
+public class imageGeneration : MonoBehaviour
+{
+    private OpenAIAPI api;
+    public string url;
+
+    void Start()
+    {
+        api = new OpenAIAPI("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");
+
+        //makeURLImages("A red dragon from mediaval times spewing fire");
+    }
+
+    public async void makeURLImages(string prompt) {
+
+        var result = await api.ImageGenerations.CreateImageAsync(prompt);
+        url = result.ToString();
+        Debug.Log(url);
+
+        //callback.Invoke();
+    }
+}
